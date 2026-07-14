71c60b2799db7a02bc5308365b04ca2b336db58d
evertonian19 <evertonian19@github.com>
2026-07-14 15:17:19 +0900

    fix/debug

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "soundcloud-tfstate-Project_sc" # <- 계정ID 붙여서 전역 유일하게
+    bucket         = "soundcloud-tfstate-201243087060" # <- 계정ID 붙여서 전역 유일하게
     key            = "prod/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "soundcloud-tflock"
