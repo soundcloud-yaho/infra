3a39012276fabe36d934f495a71f4d2067dbd42c
kimhyun <doriandko9431@gmail.com>
2026-07-30 11:48:10 +0900

    fix:토큰수정

--- terraform/environments/prod/efs.tf
+++ terraform/environments/prod/efs.tf
@@ -4,9 +4,9 @@ module "efs_prophet" {
   project_name = var.project_name
   environment  = var.environment
 
-  vpc_id                  = module.network.vpc_id
-  private_subnet_ids      = module.network.private_subnet_ids
-  node_security_group_id  = module.security.eks_node_security_group_id
+  vpc_id                 = module.network.vpc_id
+  private_subnet_ids     = module.network.private_subnet_ids
+  node_security_group_id = module.security.eks_node_security_group_id
 }
 
 output "efs_prophet_file_system_id" {
