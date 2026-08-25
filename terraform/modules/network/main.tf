28e1336b813573dcbe90d7eec0323a661cd6d5fc
kimhyun <doriandko9431@gmail.com>
2026-08-25 14:40:03 +0900

    fix: Ingress 서브넷 설정 수정

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -59,12 +59,20 @@ resource "aws_eip" "nat" {
   tags   = { Name = "${var.project_name}-${var.environment}-nat-eip" }
 }
 
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id # 첫 번째 AZ의 퍼블릭 서브넷에 배치
-  tags          = { Name = "${var.project_name}-${var.environment}-nat" }
+resource "aws_subnet" "public" {
+  count                   = length(var.availability_zones)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
-  depends_on = [aws_internet_gateway.this]
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-${var.availability_zones[count.index]}"
+
+    "kubernetes.io/role/elb" = "1"
+
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 }
 
 # ---------- 라우팅 ----------
